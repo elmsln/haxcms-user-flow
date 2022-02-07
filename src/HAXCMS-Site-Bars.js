80bf2ca833daa09e628ef268cb4ee02da1b4b2a3
Jimmy Kane <jbk5603@psu.edu>
2022-02-07 14:03:48 -0500

    Got rid of extra functions

--- src/HAXCMS-Site-Bars.js
+++ src/HAXCMS-Site-Bars.js
@@ -25,24 +25,6 @@ export class HAXCMSSiteBars extends SimpleColors {
     };
   }
 
-  // updated fires every time a property defined above changes
-  // this allows you to react to variables changing and use javascript to perform logic
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if (propName === 'need' && this[propName] === 'joy') {
-        this.classList.add('joyful');
-      }
-    });
-  }
-
-  // Lit life-cycle; this fires the 1st time the element is rendered on the screen
-  // this is a sign it is safe to make calls to this.shadowRoot
-  firstUpdated(changedProperties) {
-    if (super.firstUpdated) {
-      super.firstUpdated(changedProperties);
-    }
-  }
-
   // CSS - specific to Lit
   static get styles() {
     return css`
