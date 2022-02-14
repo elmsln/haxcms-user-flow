bc60fdfe0b9481f3c49a03665762e0fc4ae625cb
Jimmy Kane <jbk5603@psu.edu>
2022-02-14 13:15:55 -0500

    implemented lit-labs, iconLink, and font color

--- src/HAXCMS-Site-Bars.js
+++ src/HAXCMS-Site-Bars.js
@@ -3,6 +3,7 @@ import { html, css } from 'lit';
 import '@lrnwebcomponents/simple-icon/lib/simple-icons.js';
 import '@lrnwebcomponents/simple-icon/lib/simple-icon-lite.js';
 import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors';
+import { animate } from '@lit-labs/motion';
 
 // EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
 // which has the magic life-cycles and developer experience below added
@@ -18,6 +19,7 @@ export class HAXCMSSiteBars extends SimpleColors {
     this.icon = 'add';
     this.opened = false;
     this.inprogress = false;
+    this.iconLink = 'https://www.psu.edu';
   }
 
   // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
@@ -27,6 +29,7 @@ export class HAXCMSSiteBars extends SimpleColors {
       opened: { type: Boolean, reflect: true },
       icon: { type: String, reflect: true },
       inprogress: { type: Boolean, reflect: true },
+      iconLink: { type: String, reflect: true },
     };
   }
 
@@ -55,215 +58,20 @@ export class HAXCMSSiteBars extends SimpleColors {
         :host {
           --main-banner-width: 500px;
           --main-banner-height: 90px;
-          --main-banner-color: #132279;
-          --band-banner-color: #2746f8;
+          --band-banner-height: 180px;
           display: inline-block;
           background-image: linear-gradient(
             var(--simple-colors-default-theme-accent-10) 80%,
             var(--simple-colors-default-theme-accent-6)
           );
-          animation-duration: 3.5s;
+          color: var(--simple-colors-default-theme-accent-1);
         }
 
         :host([opened]) {
-          animation-name: fadegradientopen;
-          animation-duration: 3.5s;
           background-image: linear-gradient(
             var(--simple-colors-default-theme-accent-10),
             var(--simple-colors-default-theme-accent-6)
           );
-        }
-
-        @keyframes fadegradientopen {
-          0% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10),
-              var(--simple-colors-default-theme-accent-6) 5%
-            );
-          }
-
-          25% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10),
-              var(--simple-colors-default-theme-accent-6) 4%
-            );
-          }
-
-          50% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10),
-              var(--simple-colors-default-theme-accent-6) 2%
-            );
-          }
-
-          75% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10),
-              var(--simple-colors-default-theme-accent-6) 1%
-            );
-          }
-
-          100% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10),
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-        }
-
-        @keyframes fadegradientclosed {
-          0% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10),
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          6% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 5%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          12% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 10%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          18% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 15%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          25% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 20%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          31% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 25%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          37% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 30%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          42% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 35%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          50% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 40%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          56% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 45%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          62% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 50%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          68% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 55%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          71% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 58%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          75% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 60%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          78% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 62%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-          81% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 65%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          85% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 67%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          89% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 70%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          91% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 73%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          94% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 75%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          97% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 77%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
-
-          100% {
-            background-image: linear-gradient(
-              var(--simple-colors-default-theme-accent-10) 80%,
-              var(--simple-colors-default-theme-accent-6)
-            );
-          }
         }
         #mainCard {
           display: flex;
@@ -282,6 +90,14 @@ export class HAXCMSSiteBars extends SimpleColors {
           width: var(--main-banner-width);
           overflow: hidden;
         }
+
+        :host([opened]) #band {
+          display: flex;
+          flex-direction: column;
+          width: var(--main-banner-width);
+          height: var(--band-banner-height);
+        }
+
         button {
           background-color: transparent;
           border: none;
@@ -299,46 +115,27 @@ export class HAXCMSSiteBars extends SimpleColors {
         #plus {
           --simple-icon-width: 49px;
           --simple-icon-height: 49px;
+          color: var(--simple-colors-default-theme-accent-1);
         }
         #dots {
           --simple-icon-width: 49px;
           --simple-icon-height: 49px;
+          color: var(--simple-colors-default-theme-accent-1);
         }
       `,
     ];
   }
 
   __clickButton() {
-    const element = this.renderRoot.querySelector('#band');
-    if (element.style.height === '180px') {
-      element.style.height = '0px';
-      this.opened = false;
-      this.inprogress = true;
-    } else {
-      this.opened = true;
-      this.inprogress = true;
-      const height = getComputedStyle(element).getPropertyValue(
-        '--main-banner-height'
-      );
-
-      const newHeight = this.__cssPropToNumber(height);
-      element.style.height = newHeight;
-    }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  __cssPropToNumber(property) {
-    let newProperty = property.slice(0, -2);
-    newProperty *= 2;
-    return `${newProperty}px`;
+    this.opened = !this.opened;
   }
 
   // HTML - specific to Lit
   render() {
     return html`
       <div id="mainCard">
-        <a href="https://www.psu.edu"
-          ><simple-icon-lite icon=${this.icon} id="plus"></simple-icon-lite
+        <a href="${this.iconLink}">
+          <simple-icon-lite icon=${this.icon} id="plus"></simple-icon-lite
         ></a>
         <div id="labels">
           <slot name="heading"></slot>
@@ -348,7 +145,7 @@ export class HAXCMSSiteBars extends SimpleColors {
           <simple-icon-lite icon="more-vert" id="dots"></simple-icon-lite>
         </button>
       </div>
-      <div id="band">
+      <div id="band" ${animate()}>
         <slot name="band"></slot>
       </div>
     `;
