4a0d9a9c5939c51cc3b6fb00ab5744e4971fa780
btopro <btopro@outlook.com>
2022-03-02 15:58:56 -0500

    random refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { HAXCMSSiteBars } from './HAXCMS-Site-Bars.js';
 import { HAXCMSSiteDetails } from './HAXCMS-Site-Details.js';
-import { HAXCMSRandomWords } from './HAXCMS-Random-Words.js';
 
-customElements.define(HAXCMSRandomWords.tag, HAXCMSRandomWords);
 customElements.define(HAXCMSSiteBars.tag, HAXCMSSiteBars);
 customElements.define(HAXCMSSiteDetails.tag, HAXCMSSiteDetails);
