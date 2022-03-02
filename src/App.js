1453ff64349b748a06535f50f652c965ac4e7382
dronk6 <dkr5301@psu.edu>
2022-03-02 13:18:14 -0500

    In progress

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { HAXCMSSiteBars } from './HAXCMS-Site-Bars.js';
 import { WiredElementsTest } from './WiredElementsTest.js';
 import { HAXCMSSiteButton } from './HAXCMS-Site-Button.js';
 import { HAXCMSSiteDetails } from './HAXCMS-Site-Details.js';
+import { HAXCMSWelcomeBar } from './HAXCMS-Welcome-Bar.js';
 
 customElements.define(HAXCMSSiteBars.tag, HAXCMSSiteBars);
 customElements.define(WiredElementsTest.tag, WiredElementsTest);
 customElements.define(HAXCMSSiteButton.tag, HAXCMSSiteButton);
+customElements.define(HAXCMSSiteButton.tag, HAXCMSSiteButton);
 customElements.define(HAXCMSSiteDetails.tag, HAXCMSSiteDetails);
+customElements.define(HAXCMSWelcomeBar.tag, HAXCMSWelcomeBar);
