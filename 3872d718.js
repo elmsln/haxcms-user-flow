e4c2498c51569ae0097475389d85c6e7da7750fb
liljimmyk99 <49504930+liljimmyk99@users.noreply.github.com>
2022-03-21 19:15:35 +0000

    Deploying to gh-pages from  @ 0724ff517563f69a52d8a10726d0a1a1a0d4578b 🚀

---
+++ 3872d718.js
@@ -0,0 +1,5 @@
+import{s as t}from"./d6c51396.js";import{e}from"./3ce2ccf2.js";import"./77bda7c4.js";import"./7613a8bb.js";import"./c8b51b94.js";import"./5c68dd7f.js";import"./3db2acd2.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(e(t)){static get tag(){return"rich-text-editor-unlink"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.command="unlink",this.icon="mdextra:unlink",this.label="Remove Link",this.tagsList="a",this.shortcutKeys="ctrl+k"}updated(t){super.updated&&super.updated(t),t.forEach(((t,e)=>{"range"===e&&(this.commandIsToggled?this.setAttribute("disabeld","disabled"):this.removeAttribute("disabeld"))}))}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnlink};
---
+++ 4c52a541.js
@@ -0,0 +1,5 @@
+import{s as t}from"./d6c51396.js";import{e}from"./3ce2ccf2.js";import"./77bda7c4.js";import"./7613a8bb.js";import"./c8b51b94.js";import"./5c68dd7f.js";import"./3db2acd2.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
