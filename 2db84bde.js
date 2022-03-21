11bf54b5b32d82acaa34ae9292b8e85026c19643
liljimmyk99 <49504930+liljimmyk99@users.noreply.github.com>
2022-03-21 16:26:56 +0000

    Deploying to gh-pages from  @ 7aa53f8cb9efee9a6ce3eac03c45bd6d8941e22e 🚀

---
+++ 2db84bde.js
@@ -0,0 +1,5 @@
+import{s as t}from"./d5765055.js";import{e}from"./2ca96e8f.js";import"./d7f4194d.js";import"./7613a8bb.js";import"./07510617.js";import"./5c68dd7f.js";import"./81644c9d.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
