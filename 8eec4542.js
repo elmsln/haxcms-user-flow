fbb809fe14ed1a8322564cc7581d962d9e8bde1e
btopro <bto108@psu.edu>
2022-03-22 20:50:13 +0000

    Deploying to gh-pages from  @ fcc303e3ab4058ca2140e342e87840464e5a6aed 🚀

---
+++ 8eec4542.js
@@ -0,0 +1,5 @@
+import{s as e}from"./9f7a3b15.js";import{f as t}from"./f1c5b6b3.js";import"./c5c5723a.js";import"./7613a8bb.js";import"./b84ff241.js";import"./5c68dd7f.js";import"./15a177e2.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(t(e)){static get tag(){return"rich-text-editor-underline"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.fields=[{property:"confirm",title:"Underline (not recommended)",description:"Underlines can be confused with links. Use italics instead.",inputMethod:"boolean"}],this.tagsList="u",this.icon="editor:format-underlined",this.label="Underline (not recommended)",this.toggles=!0,this.command="underline",this.shortcutKeys="ctrl+u",this.value={confirm:!1}}get promptCommandVal(){this.commandVal=void 0}getValue(){return{confirm:!!this.toggled}}setToggled(){this.toggled=!!this.getPropValue("confirm")}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnderline};
