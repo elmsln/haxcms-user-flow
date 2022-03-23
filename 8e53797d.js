ab852bd570d4a69be567d9fa4c1f62f810871f87
liljimmyk99 <49504930+liljimmyk99@users.noreply.github.com>
2022-03-23 20:05:31 +0000

    Deploying to gh-pages from  @ 00adf10b3fa3a06c576d2ea6150c9179d3a49bda 🚀

---
+++ 8e53797d.js
@@ -0,0 +1,5 @@
+import{s as t}from"./8ce32c83.js";import{e}from"./4be0ba6e.js";import"./b616507a.js";import"./7613a8bb.js";import"./f329b259.js";import"./5c68dd7f.js";import"./39abc6f5.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(e(t)){static get tag(){return"rich-text-editor-unlink"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.command="unlink",this.icon="mdextra:unlink",this.label="Remove Link",this.tagsList="a",this.shortcutKeys="ctrl+k"}updated(t){super.updated&&super.updated(t),t.forEach(((t,e)=>{"range"===e&&(this.commandIsToggled?this.setAttribute("disabeld","disabled"):this.removeAttribute("disabeld"))}))}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnlink};
