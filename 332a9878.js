e74889b1acd478daf7ce55f0ecfcbcc21910dbe7
Taylor-Bracone <71147213+Taylor-Bracone@users.noreply.github.com>
2022-03-30 18:10:07 +0000

    Deploying to gh-pages from  @ bd0658f1c7152b74d8a6c0f1dbebd8585f482b15 🚀

---
+++ 332a9878.js
@@ -0,0 +1,5 @@
+import{s as t}from"./3ea152fb.js";import{e}from"./b4df45d8.js";import"./8cd7a918.js";import"./7613a8bb.js";import"./1602df7a.js";import"./5c68dd7f.js";import"./41f1e020.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
---
+++ a9fe36a1.js
@@ -0,0 +1,5 @@
+import{s as t}from"./3ea152fb.js";import{e}from"./b4df45d8.js";import"./8cd7a918.js";import"./7613a8bb.js";import"./1602df7a.js";import"./5c68dd7f.js";import"./41f1e020.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(e(t)){static get tag(){return"rich-text-editor-unlink"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.command="unlink",this.icon="mdextra:unlink",this.label="Remove Link",this.tagsList="a",this.shortcutKeys="ctrl+k"}updated(t){super.updated&&super.updated(t),t.forEach(((t,e)=>{"range"===e&&(this.commandIsToggled?this.setAttribute("disabeld","disabled"):this.removeAttribute("disabeld"))}))}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnlink};
