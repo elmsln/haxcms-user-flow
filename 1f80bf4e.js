8431f649de7d5978eb4b1d802e8dedceba4a33b9
liljimmyk99 <49504930+liljimmyk99@users.noreply.github.com>
2022-03-31 16:10:23 +0000

    Deploying to gh-pages from  @ 3346a7fee8482fdf5695be9df997b8a298856b36 🚀

---
+++ 1f80bf4e.js
@@ -0,0 +1,5 @@
+import{s as e}from"./37c9021e.js";import{f as t}from"./7d78cd26.js";import"./90a74eb6.js";import"./7613a8bb.js";import"./c4d38991.js";import"./5c68dd7f.js";import"./32973863.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(t(e)){static get tag(){return"rich-text-editor-underline"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.fields=[{property:"confirm",title:"Underline (not recommended)",description:"Underlines can be confused with links. Use italics instead.",inputMethod:"boolean"}],this.tagsList="u",this.icon="editor:format-underlined",this.label="Underline (not recommended)",this.toggles=!0,this.command="underline",this.shortcutKeys="ctrl+u",this.value={confirm:!1}}get promptCommandVal(){this.commandVal=void 0}getValue(){return{confirm:!!this.toggled}}setToggled(){this.toggled=!!this.getPropValue("confirm")}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnderline};
