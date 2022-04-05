e18a6297cbf7837f2579ab9aceda8da65301f9d4
btopro <bto108@psu.edu>
2022-04-05 23:08:22 +0000

    Deploying to gh-pages from  @ 3795fe40b1fd3606ed183c4ce0a2ccdded4f7d90 🚀

---
+++ d2a56361.js
@@ -0,0 +1,5 @@
+import{s as t}from"./432ac660.js";import{e}from"./ac712e78.js";import"./e485da0e.js";import"./5d7cbd53.js";import"./9a8d07a0.js";import"./5c68dd7f.js";import"./bee6703b.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(e(t)){static get tag(){return"rich-text-editor-unlink"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.command="unlink",this.icon="mdextra:unlink",this.label="Remove Link",this.tagsList="a",this.shortcutKeys="ctrl+k"}updated(t){super.updated&&super.updated(t),t.forEach(((t,e)=>{"range"===e&&(this.commandIsToggled?this.setAttribute("disabeld","disabled"):this.removeAttribute("disabeld"))}))}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnlink};
