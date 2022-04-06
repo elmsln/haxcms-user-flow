7ba1b49f66fed480876119b1a67fa4c65c7859c4
btopro <bto108@psu.edu>
2022-04-06 18:35:07 +0000

    Deploying to gh-pages from  @ dfed4641c6461b879e4645943f6a943ccfd43ee8 🚀

---
+++ 734a3269.js
@@ -0,0 +1,5 @@
+import{s as t}from"./0acd8c09.js";import{e}from"./f89b93be.js";import"./ed6b7475.js";import"./311cf763.js";import"./ed8063bf.js";import"./5c68dd7f.js";import"./182040e6.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(e(t)){static get tag(){return"rich-text-editor-unlink"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.command="unlink",this.icon="mdextra:unlink",this.label="Remove Link",this.tagsList="a",this.shortcutKeys="ctrl+k"}updated(t){super.updated&&super.updated(t),t.forEach(((t,e)=>{"range"===e&&(this.commandIsToggled?this.setAttribute("disabeld","disabled"):this.removeAttribute("disabeld"))}))}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnlink};
---
+++ b631796e.js
@@ -0,0 +1,5 @@
+import{s as t}from"./0acd8c09.js";import{e}from"./f89b93be.js";import"./ed6b7475.js";import"./311cf763.js";import"./ed8063bf.js";import"./5c68dd7f.js";import"./182040e6.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
