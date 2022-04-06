ab8fe919dff8ad07797fd09e2b408d9ab91262e0
btopro <bto108@psu.edu>
2022-04-06 21:30:36 +0000

    Deploying to gh-pages from  @ 15feb79aadb353b3e2e501801cef8bf124841058 🚀

---
+++ b4bc4a25.js
@@ -0,0 +1,5 @@
+import{s as e}from"./28e2b40b.js";import{f as t}from"./ded54459.js";import"./4242f0bc.js";import"./0dac7539.js";import"./32db333f.js";import"./5c68dd7f.js";import"./1b2d5ee5.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(t(e)){static get tag(){return"rich-text-editor-underline"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.fields=[{property:"confirm",title:"Underline (not recommended)",description:"Underlines can be confused with links. Use italics instead.",inputMethod:"boolean"}],this.tagsList="u",this.icon="editor:format-underlined",this.label="Underline (not recommended)",this.toggles=!0,this.command="underline",this.shortcutKeys="ctrl+u",this.value={confirm:!1}}get promptCommandVal(){this.commandVal=void 0}getValue(){return{confirm:!!this.toggled}}setToggled(){this.toggled=!!this.getPropValue("confirm")}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnderline};
---
+++ b5eb34dd.js
@@ -0,0 +1,5 @@
+import{s as t}from"./28e2b40b.js";import{e}from"./ded54459.js";import"./4242f0bc.js";import"./0dac7539.js";import"./32db333f.js";import"./5c68dd7f.js";import"./1b2d5ee5.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
