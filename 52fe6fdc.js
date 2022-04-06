ddc38adeb6e8aa67ca45276e2e408b30960825c3
btopro <bto108@psu.edu>
2022-04-06 18:39:23 +0000

    Deploying to gh-pages from  @ df5d5d5fe2bd67071369b8144e83d39f0d3f9440 🚀

---
+++ 52fe6fdc.js
@@ -0,0 +1,5 @@
+import{s as t}from"./406260ac.js";import{e}from"./2f980ba7.js";import"./cd3ca1a7.js";import"./a2fed4ba.js";import"./333dbff6.js";import"./5c68dd7f.js";import"./90d8a071.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
