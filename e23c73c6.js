6505d384824969f5da3180b17c8b795a622771ca
btopro <bto108@psu.edu>
2022-04-14 00:34:37 +0000

    Deploying to gh-pages from  @ e25356f119d97ecaf9937297cdf4906a706c412f 🚀

---
+++ e23c73c6.js
@@ -0,0 +1,5 @@
+import{s as e}from"./2a455de0.js";import{f as t}from"./35964d5a.js";import"./92722af5.js";import"./90d557e3.js";import"./a7293a12.js";import"./99f4b640.js";import"./5c68dd7f.js";import"./a309021e.js";import"./178d48b7.js";import"./43b27239.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(t(e)){static get tag(){return"rich-text-editor-underline"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.fields=[{property:"confirm",title:"Underline (not recommended)",description:"Underlines can be confused with links. Use italics instead.",inputMethod:"boolean"}],this.tagsList="u",this.icon="editor:format-underlined",this.label="Underline (not recommended)",this.toggles=!0,this.command="underline",this.shortcutKeys="ctrl+u",this.value={confirm:!1}}get promptCommandVal(){this.commandVal=void 0}getValue(){return{confirm:!!this.toggled}}setToggled(){this.toggled=!!this.getPropValue("confirm")}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnderline};
