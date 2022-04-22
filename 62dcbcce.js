1360975b537771bc282a4493469ceb61bb58f02e
liljimmyk99 <49504930+liljimmyk99@users.noreply.github.com>
2022-04-22 16:42:34 +0000

    Deploying to gh-pages from  @ 56ce1eef448c409eea906d77ceaf4bbc979834d9 🚀

---
+++ 62dcbcce.js
@@ -0,0 +1,5 @@
+import{s as e}from"./515cd7a2.js";import{f as t}from"./118942cd.js";import"./671c9912.js";import"./1c8eb46b.js";import"./5c68dd7f.js";import"./262c3545.js";import"./cc96aa96.js";import"./f099135d.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class r extends(t(e)){static get tag(){return"rich-text-editor-underline"}render(){return super.render()}static get properties(){return{...super.properties}}constructor(){super(),this.fields=[{property:"confirm",title:"Underline (not recommended)",description:"Underlines can be confused with links. Use italics instead.",inputMethod:"boolean"}],this.tagsList="u",this.icon="editor:format-underlined",this.label="Underline (not recommended)",this.toggles=!0,this.command="underline",this.shortcutKeys="ctrl+u",this.value={confirm:!1}}get promptCommandVal(){this.commandVal=void 0}getValue(){return{confirm:!!this.toggled}}setToggled(){this.toggled=!!this.getPropValue("confirm")}}window.customElements.define(r.tag,r);export{r as RichTextEditorUnderline};
---
+++ 89341a24.js
@@ -0,0 +1,5 @@
+import{s as t}from"./515cd7a2.js";import{e}from"./118942cd.js";import"./671c9912.js";import"./1c8eb46b.js";import"./5c68dd7f.js";import"./262c3545.js";import"./cc96aa96.js";import"./f099135d.js";
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */class o extends(e(t)){static get tag(){return"rich-text-editor-source-code"}render(){return super.render()}constructor(){super(),this.icon="code",this.toggled=!1,this.label="Source Code",this.labelToggled="Rich Text",this.command="viewSource",this.toggledCommand="viewSource",this.commandVal=!0,this.toggledCommandVal=!1,this.shortcutKeys="cmd+<",this.initViewSource()}static get properties(){return{...super.properties,toggled:{attribute:"toggled",type:Boolean,reflect:!0}}}get isToggled(){return this.toggled}commandCallback(t,e,o){this.toggled||this.__highlight.unwrap(this.range),this.toggled=this.__toolbar&&this.__source&&this.__source.__toolbar==this.__toolbar}}window.customElements.define(o.tag,o);export{o as RichTextEditorSourceCode};
